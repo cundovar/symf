8ff103db94c61b284772068d9eff92f6f37ceb02
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2025-07-22 13:25:41 +0200

    deploy7

--- migrations/Version20250722112458.php
+++ migrations/Version20250722112458.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250524222251 extends AbstractMigration
+final class Version20250722112458 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,12 @@ final class Version20250524222251 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
+            CREATE TABLE apropos (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) DEFAULT NULL, texte VARCHAR(500) DEFAULT NULL, texte2 VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
             CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', statut VARCHAR(255) NOT NULL, INDEX IDX_6EEAA67DA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
@@ -28,6 +34,15 @@ final class Version20250524222251 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE panier (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, produit_id INT DEFAULT NULL, quantite INT NOT NULL, INDEX IDX_24CC0DF2A76ED395 (user_id), INDEX IDX_24CC0DF2F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, img VARCHAR(255) DEFAULT NULL, prix DOUBLE PRECISION NOT NULL, stock INT DEFAULT NULL, INDEX IDX_29A5EC2712469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, adresse VARCHAR(255) NOT NULL, telephone VARCHAR(30) NOT NULL, image VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
@@ -43,6 +58,9 @@ final class Version20250524222251 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE panier ADD CONSTRAINT FK_24CC0DF2F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE produit ADD CONSTRAINT FK_29A5EC2712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)
         SQL);
     }
 
@@ -65,6 +83,15 @@ final class Version20250524222251 extends AbstractMigration
             ALTER TABLE panier DROP FOREIGN KEY FK_24CC0DF2F347EFB
         SQL);
         $this->addSql(<<<'SQL'
+            ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC2712469DE2
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE apropos
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE category
+        SQL);
+        $this->addSql(<<<'SQL'
             DROP TABLE commande
         SQL);
         $this->addSql(<<<'SQL'
@@ -72,6 +99,15 @@ final class Version20250524222251 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE panier
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE produit
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE user
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE messenger_messages
         SQL);
     }
 }
